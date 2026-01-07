24fb71ffcedefef448caf1d58fcdbbbeaa25296c
Lucas Levy <lucaslevy2@etu.unistra.fr>
2026-01-07 09:13:10 +0100

    data preparation function

--- data_preparation.py
+++ data_preparation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 DATA_PATH = "./data/"
-from feature_extraction import data_preparation
+from feature_extraction import add_features
 
 def time_step_to_minutes(time_step):
     return int(pd.Timestamp(time_step).timestamp()/60)
@@ -9,8 +9,8 @@ def time_step_to_minutes(time_step):
 def nb_consecutive_nan(dataset : pd.DataFrame, column : str):
     max = 0
     count = 0
-    for i in range(train_data.shape[0]):
-        if np.isnan(train_data[column][i]):
+    for i in range(dataset.shape[0]):
+        if np.isnan(dataset[column][i]):
             count += 1
         else:
             if count > max:
@@ -20,15 +20,12 @@ def nb_consecutive_nan(dataset : pd.DataFrame, column : str):
         max = count
     return max
 
-train_data = pd.read_csv(DATA_PATH + "X_train.csv")
-train_data = train_data.drop(columns="Unnamed: 9")
-train_labels = pd.read_csv(DATA_PATH + "Y_train.csv")
-
 # "2013-04-18T00:01:00.0"
-train_data = data_preparation(train_data)
-train_data["time_step"] = train_data["time_step"].apply(time_step_to_minutes)
-train_labels["time_step"] = train_labels["time_step"].apply(time_step_to_minutes)
-columns = ["visibility",  "temperature",  "humidity",  "humidex",  "windchill",  "wind",  "pressure"]
-for column in columns:
-    train_data[column] = train_data[column].interpolate()
-    train_data[column] = train_data[column].bfill()
+def data_preparation(dataset_train, dataset_labels):
+    dataset_train = dataset_train.drop(columns="Unnamed: 9")
+    dataset_train["minutes_since_Epoch"] = dataset_train["time_step"].apply(time_step_to_minutes)
+    dataset_labels["time_step"] = dataset_labels["time_step"].apply(time_step_to_minutes)
+    columns = ["visibility",  "temperature",  "humidity",  "humidex",  "windchill",  "wind",  "pressure"]
+    for column in columns:
+        dataset_train[column] = dataset_train[column].interpolate()
+        dataset_train[column] = dataset_train[column].bfill()
